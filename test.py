69fc13bbbba0b1dbb73b3d81c6a774a9c6f2af45
Gregory Samarxhi <97654997+Comets2224@users.noreply.github.com>
2026-04-30 15:13:24 -0700

    Added more print statements for aiding in demo

--- test.py
+++ test.py
@@ -10,13 +10,18 @@ wave0 = None
 wave1 = None
 
 serial_obj = serial.Serial('COM7', 6000000, timeout=10)
-print(f"Port: {serial_obj.name} @ {serial_obj.baudrate} baud")
+
+print(f"\n=====  Connected to: {serial_obj.name} at {serial_obj.baudrate} baud  =====\n")
+print("When sampling has begun, press CTRL+C to stop sampling.\n")
+print("During sampling, nominal current should be observed.")
+input("\n=====  Press any key to signal the Teensy to begin sampling.  =====")
 
 factor = 3.3 / ((2**10) - 1)
 
 start = time.perf_counter()
+end = time.perf_counter()
 
-while total_bytes_read < TOTAL_BYTES:
+while end - start < 1.0:
 
     serial_obj.reset_input_buffer()
     serial_obj.write(b'\x01')
@@ -29,14 +34,19 @@ while total_bytes_read < TOTAL_BYTES:
     wave0 = samples[0::2] * factor
     wave1 = samples[1::2] * factor
 
+    end = time.perf_counter()
+    print(f"{total_bytes_read} bytes ({total_bytes_read / 4} samples) read and converted in {end - start:.6f} s")
     #graph.setData(wave0)
 
-end = time.perf_counter()
+print(f"\n[TEST COMPLETE] {total_bytes_read} bytes ({total_bytes_read / 4} samples) read in {end - start:.6f} s")
+print(f"Per-Channel Sampling Frequency: ~{(total_bytes_read / 4) / (end - start):.0f} Hz")
 
-print(f"[TEST COMPLETE] {total_bytes_read} bytes ({total_bytes_read / 4} samples) read in {end - start:.6f} s")
+input("Press enter to write to the provided flash drive, peak current should be expected.")
+serial_obj.write(b'\x03')
 
 serial_obj.close()
 
+print(f"\nTest graph is generating...")
 graph = pyqtgraph.plot(y=wave0, pen='y')
 graph.plot(y=wave1, pen='c')
 
